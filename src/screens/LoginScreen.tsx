968157f9f7068f2b4032bdaaa4a042304196f9a5
Jose_Bento_Abreu <163444089+JoseBento16@users.noreply.github.com>
2026-03-23 01:58:50 -0400

    git add src/screens/LoginScreen.tsx git commit -m "feat(login): adiciona logo temporario na tela de login"

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,8 +1,12 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
+       <View style={styles.logoFake}>
+        <Text style={styles.logoTexto}>💰</Text>
+        </View>
+
       <Text style={styles.titulo}>FinanceApp</Text>
 
       <Text style={styles.subtitulo}>
@@ -18,6 +22,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 24,
   },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
   titulo: {
     fontSize: 24,
     fontWeight: 'bold',
@@ -28,4 +38,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 8,
   },
+  logoFake: {
+  width: 80,
+  height: 80,
+  borderRadius: 40,
+  backgroundColor: '#4CAF50',
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignSelf: 'center',
+  marginBottom: 20,
+},
+logoTexto: {
+  fontSize: 32,
+},
 });
